4de0b1b975e7a787e326fae8ade7b04443bcbd4d
woo972 <wzak9720>
2021-06-14 09:44:26 +0900

    lb

--- src/main/java/com/wowls/userservice/HelloController.java
+++ src/main/java/com/wowls/userservice/HelloController.java
@@ -1,15 +1,23 @@
 package com.wowls.userservice;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Slf4j
 @RestController
-@RequestMapping("/userservice")
+@RequestMapping("/user-service")
+@RequiredArgsConstructor
 public class HelloController {
+
+    private final Environment env;
+
     @GetMapping("/hello")
     public String hello() {
         return "hello user service";
@@ -28,7 +36,9 @@ public class HelloController {
     }
 
     @GetMapping("/check")
-    public String check() {
+    public String check(HttpServletRequest request)
+    {
+        log.info("port:: {} {}", env.getProperty("local.server.port"), request.getServerPort());
         return "custom filter check";
     }
 }
